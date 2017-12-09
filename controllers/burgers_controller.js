60afec2aaa4919bff47a9c1f352558fcbfe7414d
Andrew Lipsey <adlipsey@gmail.com>
2017-12-08 19:52:13 -0500

    Aesthetic changes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -22,7 +22,6 @@ router.post("/api/burgers", function(req, res) {
 
 router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
-  console.log(req.body.data);
   burger.update(req.body.data, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
